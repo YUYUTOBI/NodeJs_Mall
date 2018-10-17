4ce0b3112c0bdbda1464aeefa2ea1e7aac2206de
main wang <3224873153@qq.com>
2018-10-17 19:29:35 +0800

    Service层测试和对Service层修正

--- NodeJs_Mall/Service/user.js
+++ NodeJs_Mall/Service/user.js
@@ -2,13 +2,14 @@ let User = require("../Model/user");
 let encryptUtil = require("../Utils/encryptUtil");
 
 async function regist(user) {
-    let result = await findByUser(user.username);
+    let result = await findByUsername(user.username);
     if (result) {
         throw Error(`用户名${user.username}已经被占用`)
     }
     user.password = encryptUtil.md5Hmac(user.password, user.username)
     user.role = 0;
     result = await User.create(user);
+    result.password = "";
     return result;
 }
 
@@ -20,24 +21,33 @@ async function deleteUserByUsername(username) {
     }
 }
 
-async function findByUser(username) {
-    return User.findOne({username: username})
-
+async function findByUsername(username) {
+    return await User.findOne({username: username})
 }
 
 async function isExistByUsername(username) {
-    let result = await findByUser(username)
+    let result = await findByUsername(username)
     if (!result) {
         throw Error(`用户名为${username}的用户不存在`)
     }
 }
 
 async function login(user) {
-    await isExistByUsername(username);
+    await isExistByUsername(user.username);
     let password = user.password;
     if (password == null || password.trim().length == 0) {
         throw Error("密码不能为空");
     }
-    user.password = encryptUtil.md5Hmac(password, user.username)
-    return await User.findOne(user)
+    user.password= encryptUtil.md5Hmac(password,user.username)
+    console.log(user.password+"-------")
+    user = await User.findOne(user);
+    user.password = "";
+    return user;
+}
+
+module.exports = {
+    regist,
+    login,
+    deleteUserByUsername,
+    findByUsername
 }
---
+++ NodeJs_Mall/test/testUserService.js
@@ -0,0 +1,32 @@
+let userService = require("../Service/user");
+require("../db")
+
+
+async function testUser(){
+/*//用户添加测试
+    let user={
+        username:"lisi",
+        password:"12345",
+        role:100
+    }
+    user=await userService.regist(user)
+    console.log(user)*/
+
+/*
+//登录测试
+let user= {
+    username: "李四",
+    password: "12345"
+}
+let userlogin=await userService.login(user)
+    console.log(userlogin)*/
+
+/*
+//删除测试
+  userService.deleteUserByUsername("李四");*/
+/*
+//查询测试
+  let user= await userService.findByUsername("lisi")
+    console.log(user)*/
+}
+testUser()
